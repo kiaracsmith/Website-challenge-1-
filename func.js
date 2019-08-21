e69a79932b6777b0134171470f75987d4adb36af
kiaracsmith <45629725+kiaracsmith@users.noreply.github.com>
2019-08-21 21:41:28 +0200

    Update func.js

--- func.js
+++ func.js
@@ -1,9 +1,13 @@
 var global = 0;
 var x = 0;
+
+// adds number using button
 function add(){
   document.getElementById("cont").innerHTML++;
   global = document.getElementById("cont").innerHTML;
 }
+
+//subtracts number using button
 function minus(){
   if(document.getElementById("cont").innerHTML <= 0){
     return;
@@ -12,6 +16,7 @@ function minus(){
   global = document.getElementById("cont").innerHTML;
 }
 
+// resets the number if clicked "cancel"
 function reset(){
     document.getElementById("cont").innerHTML = 0;
     global = 0;
@@ -20,6 +25,7 @@ function reset(){
     document.getElementById("mod").innerHTML = "Add to Cart";
 }
 
+// applies the number to the page if clicked "accept"
 function apply(){
     x = global;
     document.getElementById("result").innerHTML = x;
